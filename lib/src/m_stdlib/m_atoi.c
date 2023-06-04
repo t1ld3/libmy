0ee6f55717fd852b1a4cc7844c89517a63a5e291
T1ld3 <71977639+t1ld3@users.noreply.github.com>
2023-06-04 23:50:16 +0200

    fix: use pointers to fix implementation of m_atoi

--- lib/src/m_stdlib/m_atoi.c
+++ lib/src/m_stdlib/m_atoi.c
@@ -17,24 +17,24 @@ static int m_int_signedmax(int sign)
 
 int m_atoi_base(char const *str, char const *base)
 {
-    int base_l = m_strlen(base);
+    char *digit = NULL;
+    int base_l = 0;
     int ret = 0;
     int sign = 1;
 
-    while (*str == ' ') {
+    if (str == NULL || base == NULL)
+        return INT_MIN;
+    base_l = m_strlen(base);
+    while (*str == ' ')
         str++;
-    }
     if (*str == '-' && sign > 0) {
         sign *= -1;
         str++;
     }
-    while (m_strchr(base, *str) != NULL) {
-        if (ret > INT_MAX / 10
-            || (ret == INT_MAX / 10 && *str - base[0] > 7)) {
+    while ((digit = m_strchr(base, *str)) != NULL && *digit != '\0' && str++) {
+        if (ret > INT_MAX / 10 || (ret == INT_MAX / 10 && digit - base > 7))
             return m_int_signedmax(sign);
-        }
-        ret = (ret * base_l) + *str - base[0];
-        str++;
+        ret = (ret * base_l) + (digit - base);
     }
     return ret * sign;
 }
